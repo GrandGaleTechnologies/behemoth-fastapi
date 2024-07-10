d817283a5898339950b98ec98f1fb2f736a6a4c1
Bello Shehu Ango <angobello0@gmail.com>
2024-07-10 12:02:45 +0100

    app: add db url

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,15 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
+from app.core.settings import get_settings
+
+settings = get_settings()
+
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.POSTGRES_DATABASE_URL)
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
