6cca2f07b96d213435bbec1bd119e7d8536bf2fc
Akashi <148244536+Akashii0@users.noreply.github.com>
2025-05-08 21:05:49 +0100

    feat: auto-module
    * ADD: module creation automation for FastAPI with boilerplate setup
    
    * doc: add usage instructions for auto-module.py script in README
    
    * chore: minor improvements
    
    ---------
    
    Co-authored-by: Bello Shehu Ango <angobello0@gmail.com>

---
+++ auto-module.py
@@ -0,0 +1,93 @@
+import textwrap
+from pathlib import Path
+
+
+def create_module():
+    """
+    Automates the creation of a FastAPI module with necessary boilerplate
+    """
+    app_dir = Path("./app")
+
+    if not app_dir.exists():
+        print("App directory does not exist. Creating one...")
+        app_dir.mkdir(parents=True)
+        (app_dir / "__init__.py").touch()
+
+    module = input("What is the module name? ").strip().lower()
+    module_path = app_dir / module
+
+    if module_path.exists():
+        print("Directory already exists.")
+        return
+
+    print(f"Creating the module: {module=}")
+    module_path.mkdir()
+
+    # Create subfolders and __init__.py files
+    sub_folders = ["routes", "schemas"]
+    for folder in sub_folders:
+        sub_path = module_path / folder
+        sub_path.mkdir()
+        (sub_path / "__init__.py").touch()
+        (sub_path / "base.py").touch()
+
+    # Create specific schema files
+    schema_files = ["create.py", "edit.py", "response.py"]
+    schemas_path = module_path / "schemas"
+    for schema_file in schema_files:
+        (schemas_path / schema_file).touch()
+
+    # Define files and templates
+    files = [
+        "__init__.py",
+        "routes\\base.py",
+        "apis.py",
+        "models.py",
+        "services.py",
+        "selectors.py",
+        "exceptions.py",
+        "formatters.py",
+    ]
+
+    # NOTE: we add \ after the """ to escape and not have a \n newline
+    file_templates = {
+        # routes/base.py
+        "routes\\base.py": """\
+        from fastapi import APIRouter
+
+        # Globals
+        router = APIRouter()
+        """,
+        # apis.py
+        "apis.py": f"""\
+        from fastapi import APIRouter
+
+        from app.core.tags import RouteTags
+        from app.{module}.routes.base import router as base_router
+
+        # Globals
+        router = APIRouter()
+        tags = RouteTags()
+
+        # Routes
+        router.include_router(base_router, prefix="/{module.lower()}s")
+        """,
+        # Future example for services.py
+        # "services.py": f"# Services for {module} module\n",
+    }
+
+    # Write or touch files
+    for file in files:
+        file_path = module_path / file
+        content = file_templates.get(file)
+        if content:
+            file_path.write_text(textwrap.dedent(content))
+        else:
+            print(file_path)
+            file_path.touch()
+
+    print(f"✅ Module '{module}' created successfully")
+
+
+if __name__ == "__main__":
+    create_module()
