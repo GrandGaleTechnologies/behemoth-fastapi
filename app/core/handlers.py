d3520738a6e756d51193255a56611c068f69fd5f
Bello Shehu Ango <angobello0@gmail.com>
2025-05-12 12:47:39 +0100

    chore: default print custom exceptions

--- app/core/handlers.py
+++ app/core/handlers.py
@@ -18,14 +18,8 @@ async def base_exception_handler(_: Request, exc: Exception):
     """
     Exception handler for general Exception
     """
-    # Send email to staff
-    if settings.DEBUG:
-        print(exc)
-    else:
-        # Send email to staff
-        # sendgrid.send_email()
-        ...
-
+    # OPTIONAL: send email to staff
+    print(exc)
     return ORJSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         content=jsonable_encoder(
@@ -62,14 +56,8 @@ async def internal_server_error_exception_handler(_: Request, exc: InternalServe
     """
     Exception handler for 'InternalServerError' exception
     """
-    # Send email to staff
-    if settings.DEBUG:
-        print(exc)
-    else:
-        # Send staff a notice
-        # sendgrid.send_email()
-        ...
-
+    # OPTIONAL: send email to staff
+    print(exc)
     return ORJSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         content=jsonable_encoder(
@@ -86,13 +74,8 @@ async def bad_gateway_error_exception_handler(_: Request, exc: BadGatewayError):
     """
     Exception handler for 'BadGatewayError' exception
     """
-    if settings.DEBUG:
-        print(exc)
-    else:
-        # Send email to staff
-        # sendgrid.send_email()
-        ...
-
+    # OPTIONAL: send email to staff
+    print(exc)
     return ORJSONResponse(
         status_code=status.HTTP_502_BAD_GATEWAY,
         content=jsonable_encoder(
